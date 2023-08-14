75b3c79faa3599f404e7e31fbf98f48b73edfda0
umarahgul <umarah.gul@gmail.com>
2023-08-15 01:18:53 +0500

    addede evenListenerLibrary.js module

---
+++ modules/eventListenersLibrary.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const booksCollection = new Library();
+    booksCollection.initializeLocalStorage();
+  
+    const addBtn = document.getElementById('addBtn');
+    addBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const titleInput = document.getElementById('title');
+      const authorInput = document.getElementById('author');
+      booksCollection.addData(titleInput.value, authorInput.value);
+      titleInput.value = '';
+      authorInput.value = '';
+    });
+  
+    booksCollection.showBooks();
+  });
+  
