afd5b78031ea84cf715a1244061f5ed2aeb0c263
umarahgul <umarah.gul@gmail.com>
2023-08-15 23:37:47 +0500

    removed eslint errors from displaydate.js module

--- modules/displayDate.js
+++ modules/displayDate.js
@@ -1,21 +1,21 @@
-import  {DateTime}  from './luxon.min.js';
+import { DateTime } from './luxon.min.js';
 
 const displayCurrentDate = () => {
-    const dateElement = document.querySelector('#date');
-  
-    const currentDate = DateTime.local();
-  
-    const formattedDate = currentDate.toLocaleString({
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true,
-    });
-  
-    dateElement.innerHTML = formattedDate;
-  }
+  const dateElement = document.querySelector('#date');
+
+  const currentDate = DateTime.local();
+
+  const formattedDate = currentDate.toLocaleString({
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  });
+
+  dateElement.innerHTML = formattedDate;
+};
 
 export default displayCurrentDate;
